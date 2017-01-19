37c6f327005d6ba3b8b196ad178468303be7d289
Nathan Scully <nathan.r.scully@gmail.com>
2017-01-19 11:14:04 +1100

    moved the AWS config checks around to allow for defaults at runtime

--- startup/entrypoint.sh
+++ startup/entrypoint.sh
@@ -3,13 +3,15 @@
 # The AWS ID & Secrets and bucket name are required to be passed in at run time as Environmental Vairables to clone the bucket contents down to the zeppelin/conf folder.
 
 
-: "${AWS_SECRET_ACCESS_KEY?You need to set AWS_SECRET_ACCESS_KEY}"
-: "${AWS_ACCESS_KEY_ID?You need to set AWS_ACCESS_KEY_ID}"
-: "${ZEPPELIN_CONF_S3_BUCKET?You need to set ZEPPELIN_CONF_S3_BUCKET}"
-
 if [[ -n ${AWS_SECRET_ACCESS_KEY} ]] && [[ -n ${AWS_ACCESS_KEY_ID} ]] && [[ -n ${ZEPPELIN_CONF_S3_BUCKET} ]]; then
   echo "Cloning zeppelin config directory from S3..."
   aws s3 sync s3://${ZEPPELIN_CONF_S3_BUCKET} ${ZEPPELIN_HOME}/conf
-  echo "Starting up Zeppelin..."
-  (${ZEPPELIN_HOME}/bin/zeppelin.sh)
+else
+  echo "AWS Config clone not occuring, using default configuration."
+  : "${AWS_SECRET_ACCESS_KEY?You need to set AWS_SECRET_ACCESS_KEY}"
+  : "${AWS_ACCESS_KEY_ID?You need to set AWS_ACCESS_KEY_ID}"
+  : "${ZEPPELIN_CONF_S3_BUCKET?You need to set ZEPPELIN_CONF_S3_BUCKET}"
 fi
+
+echo "Starting up Zeppelin..."
+(${ZEPPELIN_HOME}/bin/zeppelin.sh)
