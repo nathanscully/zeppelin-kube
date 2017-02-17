4bbb07d531f04e1c4f057df0f79c6bc6db15a07d
Greg Cook <greg@gregcook.com>
2017-02-17 11:49:38 +1100

    update Kubernetes files, add nginx proxy

--- kubernetes/kops-create-cluster.sh
+++ kubernetes/kops-create-cluster.sh
@@ -3,8 +3,12 @@ kops create cluster \
   --zones=ap-southeast-2b,ap-southeast-2c \
   --master-zones=ap-southeast-2b \
   --node-count=2 \
-  --node-size=m4.large \
+  --node-size=r4.xlarge \
   --master-size=t2.micro \
   --vpc=vpc-dbd607bf \
   --network-cidr=10.6.0.0/16 \
+  --associate-public-ip=false \
+  --admin-access=59.100.238.210/32 \
+  --networking=weave \
+  --topology=private \
   ${NAME}
