cb20a7f53025a545deda341ee3f2dd1cd9a5ec36
Nathan Scully <nathan.r.scully@gmail.com>
2017-02-17 17:24:48 +1100

    AWS credentials for spark + parrallel mvn

--- startup/entrypoint.sh
+++ startup/entrypoint.sh
@@ -2,6 +2,10 @@
 # This script clones the Zeppelin config directory from an S3 Bucket which contains the nessecary secrets.
 # The AWS ID & Secrets and bucket name are required to be passed in at run time as Environmental Variables to clone the bucket contents down to the zeppelin/conf folder.
 
+function add_aws_to_spark_env {
+  echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> /usr/spark-2.0.2/conf/spark-env.sh
+  echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> /usr/spark-2.0.2/conf/spark-env.sh
+}
 
 if [ "$1" = "zeppelin" ] ; then
   if [[ -n ${AWS_SECRET_ACCESS_KEY} ]] && [[ -n ${AWS_ACCESS_KEY_ID} ]] && [[ -n ${ZEPPELIN_CONF_S3_BUCKET} ]]; then
@@ -11,14 +15,17 @@ if [ "$1" = "zeppelin" ] ; then
     echo "AWS Config clone not occuring, using default configuration."
     echo "The following Environmental Variables need to be set: AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID and ZEPPELIN_CONF_S3_BUCKET"
   fi
+  add_aws_to_spark_env
   echo "Starting up Zeppelin..."
   (${ZEPPELIN_HOME}/bin/zeppelin.sh)
 fi
 if [ "$1" = "spark-master" ] ; then
+  add_aws_to_spark_env
   echo "Starting up Spark Master..."
   ($SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master)
 fi
 if [ "$1" = "spark-worker" ] ; then
+  add_aws_to_spark_env
   echo "Starting up Spark Worker..."
   ($SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker spark://sparkmaster:7077)
 fi
