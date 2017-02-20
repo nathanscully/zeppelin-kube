830e2c7c19e4a0d4104306af0cb691de1e539a16
Greg Cook <greg@gregcook.com>
2017-02-20 15:45:42 +1100

    finalise deploy script, add volume to job

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -9,59 +9,12 @@ echo ""
 
 # Set defaults if no input
 if [[ -z "$tag" ]]; then
-  branch="latest"
+  tag="latest"
   fi
 
 # debug
-echo $tag
-sleep 120
-
-# Allow case insensitive matching
-shopt -s nocasematch
-
-# Set branches to use for each app, exit if invalid
-# if [[ $app == 'Site' ]]; then
-#   if [[ ! -z "$branch" ]]; then
-#     atlas_branch="latest"
-#     directory_branch="latest"
-#     site_branch="$branch"
-#     widget_branch="latest"
-#   else echo "Oops, invalid input, try again..." \
-#     && exit
-#   fi
-# elif [[ $app == "Directory" ]]; then
-#   if [[ ! -z "$branch" ]]; then
-#     atlas_branch="latest"
-#     directory_branch=$branch
-#     site_branch="latest"
-#     widget_branch="latest"
-#   else echo "Oops, invalid input, try again..." \
-#     && exit
-#   fi
-# elif [[ $app == "Atlas" ]]; then
-#   if [[ ! -z "$branch" ]]; then
-#     atlas_branch=$branch
-#     directory_branch="latest"
-#     site_branch="latest"
-#     widget_branch="latest"
-#   else echo "Oops, invalid input, try again..." \
-#     && exit
-#   fi
-# elif [[ $app == "Widget" ]]; then
-#   if [[ ! -z "$branch" ]]; then
-#     atlas_branch="latest"
-#     directory_branch="latest"
-#     site_branch="latest"
-#     widget_branch=$branch
-#   else echo "Oops, invalid input, try again..." \
-#     && exit
-#   fi
-# else echo "Oops, invalid input, try again..." \
-#   && exit
-# fi
-
-# Return case matching to normal
-shopt -u nocasematch
+# echo $tag
+# sleep 20
 
 # Allow user to verify correct versions before deploying
 echo ""
@@ -79,19 +32,7 @@ if [[ $correct == 'n' ]]; then
   exit
 fi
 
-# echo ""
-# echo "#####   This will take about 10 minutes. Ping Pong?    #####"
-# echo ""
-# echo "ICAgICAgICAgIE8gLgogICAgICAgIF8vfFxfLU8KICAgICAgIF9fX3xfX19fX19f
-# CiAgICAgIC8gICAgIHwgICAgIFwKICAgICAvICAgICAgfCAgICAgIFwKICAgICMj
-# IyMjIyMjIyMjIyMjIyMjCiAgIC8gICBfICggKXwgICAgICAgIFwKICAvICAgKCAp
-# IHwgfCAgICAgICAgIFwKIC8gIFwgIHxfLyAgfCAgICAgICAgICBcCi9fX19fXC98
-# X19fX3xfX19fX19fX19fX1wKICAgfCAgIHwgICAgICAgICAgICAgfAogICB8ICAv
-# IFwgICAgICAgICAgICB8CiAgIHwgLyAgIFwgICAgICAgICAgIHwKICAgXy8gICAg
-# L18K" | base64 --decode
-# echo ""
-
-sleep 5
+sleep 1
 
 echo ""
 echo "##### Creating namespace and setting up kubectl #####"
@@ -109,53 +50,44 @@ echo ""
 echo "##### Creating dependencies #####"
 echo ""
 
-# Create configmaps
-# echo "##### ConfigMaps..."
-# kubectl create -f configmaps/atlas.yaml
-# cat configmaps/site.yaml | sed "s/branch.staging/$branch.staging/g" | kubectl create -f -
-# cat configmaps/widget.yaml | sed "s/branch.staging/$branch.staging/g" | kubectl create -f -
-
 # Create secrets
 echo "##### Secrets..."
 kubectl create -f secrets/dockerhub.yaml
 kubectl create -f secrets/zeppelin-deploy-secrets.yaml
 
-# Create persistent volume claim
+# Create storage definition and persistent volume claim
 echo "##### Storage..."
 kubectl create -f storage/aws-ebs.yaml
 kubectl create -f storage/zeppelin-pvc.yaml
+sleep 5
 
-# Create dependencies
-# kubectl create -f deployments/dependencies.yaml
-# echo ""
-# echo "##### Waiting 2 minutes for pods to be ready #####"
-# echo ""
-# sleep 120
-
-# Run db:setup
+# Copy default conf files to persistent volume
+echo "##### Creating job to copy default conf..."
 cat jobs/zeppelin-copy-config.yaml | sed "s/insights-zeppelin-spark:latest/insights-zeppelin-spark:$tag/g" | kubectl create -f -
-# echo ""
-# echo "##### Waiting 5 minutes for db:setup to complete #####"
-# echo ""
-# sleep 300
+echo ""
+echo "##### Waiting 1 minute for job to complete #####"
+echo ""
+sleep 60
 
 # Delete job
+echo "##### Remove job..."
 cat jobs/zeppelin-copy-config.yaml | sed "s/insights-zeppelin-spark:latest/insights-zeppelin-spark:$tag/g" | kubectl delete -f -
 
 # Create rest of environment
+echo "##### Creating pods..."
 cat deployments/zeppelin.yaml | sed "s/insights-zeppelin-spark:latest/insights-zeppelin-spark:$tag/g" | kubectl create -f -
 cat deployments/spark-master.yaml | sed "s/insights-zeppelin-spark:latest/insights-zeppelin-spark:$tag/g" | kubectl create -f -
 cat deployments/spark-worker.yaml | sed "s/insights-zeppelin-spark:latest/insights-zeppelin-spark:$tag/g" | kubectl create -f -
 cat deployments/nginx-proxy.yaml | sed "s/insights-zeppelin-nginx:latest/insights-zeppelin-nginx:$tag/g" | kubectl create -f -
 
 echo ""
-echo "##### Waiting 3 minutes for remaining pods to be ready #####"
+echo "##### Waiting 2 minutes for remaining pods and LoadBalancer to be ready #####"
 echo ""
-sleep 180
+sleep 120
 
 echo "Your testing environment is now ready."
 echo "URL is below"
-kubectl describe service nginx
+kubectl describe service nginx | grep "LoadBalancer Ingress"
 echo ""
 
 exit
